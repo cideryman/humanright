a21947e38bdd6d63b1ca6e0b835bbcd4de963f75
Jongkyun Kim <qnftk0702@gmail.com>
2026-05-01 22:49:26 +0900

    Refresh PWA cache after updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rights-board-v1";
+const CACHE_NAME = "rights-board-v2";
 
 // Icon files expected by manifest.json:
 // ./icons/icon-192.png
@@ -13,6 +13,21 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name)),
+        ),
+      )
+      .then(() => self.clients.claim()),
   );
 });
 
